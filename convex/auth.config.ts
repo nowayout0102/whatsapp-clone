73dd7f812421e9dec33eaae75617aaca70b27534
nowayout0102 <huykhoale0104@gmail.com>
2024-09-23 12:24:02 +0700

    2-Webhooks added

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig ={
+    providers: [
+      {
+        domain: "https://powerful-bullfrog-70.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+};
+
+export default authConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,4 +14,5 @@ export default function Home() {
 			</div>
 		</main>
 	);
-}
+};
+
