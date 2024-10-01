665ede9b6e27193d3dd9b73d75d28e62252bdae1
nowayout0102 <huykhoale0104@gmail.com>
2024-10-01 22:35:21 +0700

    3-getMyConversations query added

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,5 @@
 import {defineSchema, defineTable} from 'convex/server';
 import { v } from 'convex/values';
-import { text } from 'stream/consumers'
 
 export default defineSchema({
     users: defineTable({
@@ -17,5 +16,12 @@ export default defineSchema({
 		groupName: v.optional(v.string()),
 		groupImage: v.optional(v.string()),
 		admin: v.optional(v.id("users"))
-	})
+	}),
+
+	messages: defineTable({
+		conversation: v.id("conversations"),
+		sender: v.string(),
+		content: v.string(),
+		messageType: v.union(v.literal("text"), v.literal("image"), v.literal("video")),
+	}).index("by_conversation", ["conversation"]),
 });
